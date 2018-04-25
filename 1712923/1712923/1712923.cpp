43d2cdc721505ea0ccc50f700f01615ffa3e3971
Pham Le Hoai Vu <kortomomolu678@gmail.com>
2018-04-25 14:52:37 +0700

    Chinh Sua

--- 1712923/1712923/1712923.cpp
+++ 1712923/1712923/1712923.cpp
@@ -1,94 +1,82 @@
 ﻿#include <stdio.h>
 #include <string.h>
-
-struct Student{
-	char *MSSV;
-	char *HoTen;
-	char *Khoa;
-	char *KHoc;
-	char *NgaySinh;
-	char *HACN;
-	char *MTBT;
-	char **SoThich;
-};
-
-struct Student Students[256];
-int readStudentFile();
-
-
-int main(int argc, char **argv)
+typedef struct
 {
-	readStudentFile();
-	return 0;
+	char *MSSV;
+	char *HoVaTen;
+	char *Khoa;
+	char *KhoaHoc;
+	char *NgaySinh;
+	char *HinhAnhCaNhan;
+	char *MoTaBanThan;
+	char *SoThich;
+}SinhVien;
+
+char * DocThongTin(FILE*fp, char*s)
+{
+	if (feof(fp))
+	{
+		return NULL;
+	}
+	fseek(fp,1,0);
+	s[0] = 0; 
+	int i = 0, c;
+
+	do
+	{
+		c = fgetc(fp);
+		if (c == '"')
+		{
+			break;
+		}
+		s[i] = c;
+		i++;
+	} while (1);
+	s[i] = 0;
+	return s;
 }
 
-int readStudentFile()
+int LayThongTin(FILE*fp, SinhVien& sv)
 {
+	DocThongTin(fp, sv.MSSV);
+	DocThongTin(fp, sv.HoVaTen);
+	DocThongTin(fp, sv.Khoa);
+	DocThongTin(fp, sv.KhoaHoc);
+	DocThongTin(fp, sv.NgaySinh);
+	DocThongTin(fp, sv.SoThich);
+	return 1;
+}
 
-	FILE *StudentFile;
-	char *buf = new char[256];
-	char *tmp;
-
-	//Lỗi nếu không cấp phát được bộ nhớ cho buf
-	if (buf == NULL) {
-		printf("No memory\n");
-		return 1;
-	}
-
-	if ((StudentFile = fopen("ac.csv", "r")) == NULL) //Đọc file
+void LaySinhVien(FILE*fp, SinhVien DS[], int&n)
+{
+	SinhVien sv; n = 0;
+	while (!feof(fp))
 	{
-		printf("File could not be opened.\n");
-	}
-
-	int i = 0;
-	int j = 0;
-	int flag = 0;
-	while (fgets(buf, 255, StudentFile) != NULL)
-	{
-		if ((strlen(buf)>0) && (buf[strlen(buf) - 1] == '\n'))
-			buf[strlen(buf) - 1] = '\0';
-
-		tmp = strtok(NULL, ",");
-		Students[i].MSSV = tmp;
-
-		tmp = strtok(NULL, ",");
-		Students[i].HoTen = tmp;
-
-		tmp = strtok(NULL, ",");
-		Students[i].Khoa = tmp;
-
-		tmp = strtok(NULL, ",");
-		Students[i].KHoc = tmp;
-
-		tmp = strtok(NULL, ",");
-		Students[i].NgaySinh = tmp;
-
-		tmp = strtok(NULL, ",");
-		Students[i].HACN = tmp;
-
-		tmp = strtok(NULL, ",");
-		for (j; strcpy(tmp, '\0') != 0; j++)
+		if (LayThongTin(fp, sv))
 		{
-			Students[i].SoThich[j] = tmp;
+			DS[n] = sv;
+			n++;
 		}
-		//tempStudent.ID = atoi(buf);
+		else break;
+	}
+}
 
-		i++;
-		flag = -1;
-	}
-	//free(buf);
-	fclose(StudentFile);
-	if (flag == -1)
-	for (i = 0; i <= sizeof(Students); i++)
+void In(FILE*fp, SinhVien DS[], int n)
+{
+	for (int i = 0; i < n; i++)
 	{
-		if (Students[i].MSSV != 0)
-			printf("MSSV: %s\n", Students[i].MSSV);
-		printf("Ho ten: %s\n", Students[i].HoTen);
-		printf("Khoa: %s\n", Students[i].Khoa);
-		printf("Khoa' hoc: %d\n", Students[i].KHoc);
-		printf("Ngay sinh: %s\n", Students[i].NgaySinh);
-		for (int k = 0; k<j; k++)
-			printf("So thich: %s\n", Students[i].SoThich);
+		fprintf(fp, "%s/t%15s %7s\t %s\t %s\t %s\n", DS[i].MSSV, DS[i].HoVaTen, DS[i].Khoa, DS[i].KhoaHoc, DS[i].NgaySinh, DS[i].SoThich);
 	}
-	return 0;
+}
+
+void main()
+{
+	SinhVien DS[10]; int n;
+	FILE*fp = fopen("Book1.csv", "rt");
+	if (fp == NULL)
+	{
+		return;
+	}
+	LaySinhVien(fp, DS, n);
+	In(fp, DS, n);
 }
