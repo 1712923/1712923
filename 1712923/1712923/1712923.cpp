135c28136b4b8727dbba22d9ae82b96aa34f98e7
Pham Le Hoai Vu <kortomomolu678@gmail.com>
2018-04-26 23:55:07 +0700

    Chinh Sua Moi

--- 1712923/1712923/1712923.cpp
+++ 1712923/1712923/1712923.cpp
@@ -1,82 +1,406 @@
 ﻿#include <stdio.h>
 #include <string.h>
+#include <wchar.h>
+#include <stdlib.h>
+#include <fcntl.h> 
+#include <io.h> 
+
 typedef struct
 {
-	char *MSSV;
-	char *HoVaTen;
-	char *Khoa;
-	char *KhoaHoc;
-	char *NgaySinh;
-	char *HinhAnhCaNhan;
-	char *MoTaBanThan;
-	char *SoThich;
+	wchar_t* MSSV;
+	wchar_t* HoTen;
+	wchar_t* khoa;
+	wchar_t* KhoaHoc;
+	wchar_t* NgaySinh;
+	wchar_t* HinhAnh;
+	wchar_t* MoTaBanThan;
+	wchar_t* SoThich;
 }SinhVien;
 
-char * DocThongTin(FILE*fp, char*s)
+int LayViTri(FILE* f, wchar_t kitu)
 {
-	if (feof(fp))
+	wchar_t c;
+	int i = 0;
+	while (!feof(f))
 	{
-		return NULL;
-	}
-	fseek(fp,1,0);
-	s[0] = 0; 
-	int i = 0, c;
-
-	do
-	{
-		c = fgetc(fp);
-		if (c == '"')
+		c = fgetwc(f);
+		if (c != kitu)
 		{
-			break;
+			i++;
 		}
-		s[i] = c;
+		else
+		{
+			return i + 1;
+		}
+	}
+	return i;
+}
+void DocThongTin(FILE* FileIn, SinhVien DS[], int &BatDau)
+{
+
+	int a, b;
+	fseek(FileIn, BatDau, SEEK_SET);
+	if (fgetwc(FileIn) != L'"')
+	{
+		fseek(FileIn, BatDau, SEEK_SET);
+		a = LayViTri(FileIn, L',');
+		DS-> MSSV = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, BatDau, SEEK_SET);
+		fgetws(DS-> MSSV, a, FileIn);
+		b = ftell(FileIn);
+	}
+	else
+	{
+		fseek(FileIn, BatDau + 1, SEEK_SET);
+		a = LayViTri(FileIn, L'"');
+		DS-> MSSV = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, BatDau + 1, SEEK_SET);
+		fgetws(DS-> MSSV, a, FileIn);
+		b = ftell(FileIn) + 1;
+	}
+	
+
+	fseek(FileIn, b + 1, SEEK_SET);
+	if (fgetwc(FileIn) != L'"')
+	{
+		fseek(FileIn, b + 1, SEEK_SET);
+		a = LayViTri(FileIn, L',');
+		DS-> HoTen = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 1, SEEK_SET);
+		fgetws(DS-> HoTen, a, FileIn);
+		b = ftell(FileIn);
+	}
+	else
+	{
+		fseek(FileIn, b + 2, SEEK_SET);
+		a = LayViTri(FileIn, L'"');
+		DS-> HoTen = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 2, SEEK_SET);
+		fgetws(DS-> HoTen, a, FileIn);
+		b = ftell(FileIn) + 1;
+	}
+	
+
+
+	fseek(FileIn, b + 1, SEEK_SET);
+	if (fgetwc(FileIn) != L'"')
+	{
+		fseek(FileIn, b + 1, SEEK_SET);
+		a = LayViTri(FileIn, L',');
+		DS-> khoa = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 1, SEEK_SET);
+		fgetws(DS-> khoa, a, FileIn);
+		b = ftell(FileIn);
+	}
+	else
+	{
+		fseek(FileIn, b + 2, SEEK_SET);
+		a = LayViTri(FileIn, L'"');
+		DS-> khoa = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 2, SEEK_SET);
+		fgetws(DS-> khoa, a, FileIn);
+		b = ftell(FileIn) + 1;
+	}
+	
+
+
+	fseek(FileIn, b + 1, SEEK_SET);
+	if (fgetwc(FileIn) != L'"')
+	{
+		fseek(FileIn, b + 1, SEEK_SET);
+		a = LayViTri(FileIn, L',');
+		DS-> KhoaHoc = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 1, SEEK_SET);
+		fgetws(DS-> KhoaHoc, a, FileIn);
+		b = ftell(FileIn);
+	}
+	else
+	{
+		fseek(FileIn, b + 2, SEEK_SET);
+		a = LayViTri(FileIn, L'"');
+		DS-> KhoaHoc = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 2, SEEK_SET);
+		fgetws(DS-> KhoaHoc, a, FileIn);
+		b = ftell(FileIn) + 1;
+	}
+	
+
+
+	fseek(FileIn, b + 1, SEEK_SET);
+	if (fgetwc(FileIn) != L'"')
+	{
+		fseek(FileIn, b + 1, SEEK_SET);
+		a = LayViTri(FileIn, L',');
+		DS-> NgaySinh = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 1, SEEK_SET);
+		fgetws(DS-> NgaySinh, a, FileIn);
+		b = ftell(FileIn);
+	}
+	else
+	{
+		fseek(FileIn, b + 2, SEEK_SET);
+		a = LayViTri(FileIn, L'"');
+		DS-> NgaySinh = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 2, SEEK_SET);
+		fgetws(DS-> NgaySinh, a, FileIn);
+		b = ftell(FileIn) + 1;
+	}
+	
+
+
+	fseek(FileIn, b + 1, SEEK_SET);
+	if (fgetwc(FileIn) != L'"')
+	{
+		fseek(FileIn, b + 1, SEEK_SET);
+		a = LayViTri(FileIn, L',');
+		DS-> HinhAnh = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 1, SEEK_SET);
+		fgetws(DS-> HinhAnh, a, FileIn);
+		b = ftell(FileIn);
+	}
+	else
+	{
+		fseek(FileIn, b + 2, SEEK_SET);
+		a = LayViTri(FileIn, L'"');
+		DS-> HinhAnh = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 2, SEEK_SET);
+		fgetws(DS-> HinhAnh, a, FileIn);
+		b = ftell(FileIn) + 1;
+	}
+	
+
+
+	fseek(FileIn, b + 1, SEEK_SET);
+	if (fgetwc(FileIn) != L'"')
+	{
+		fseek(FileIn, b + 1, SEEK_SET);
+		a = LayViTri(FileIn, L',');
+		DS-> MoTaBanThan = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 1, SEEK_SET);
+		fgetws(DS-> MoTaBanThan, a, FileIn);
+		b = ftell(FileIn);
+	}
+	else
+	{
+		fseek(FileIn, b + 2, SEEK_SET);
+		a = LayViTri(FileIn, L'"');
+		DS-> MoTaBanThan = (wchar_t*)malloc(a*sizeof(wchar_t));
+		fseek(FileIn, b + 2, SEEK_SET);
+		fgetws(DS-> MoTaBanThan, a, FileIn);
+		b = ftell(FileIn) + 1;
+	}
+	
+
+	fseek(FileIn, b, SEEK_SET);
+	if (fgetwc(FileIn) == L','){
+		if (fgetwc(FileIn) != L'"')
+		{
+			fseek(FileIn, b + 1, SEEK_SET);
+			a = LayViTri(FileIn, '\n');
+			DS-> SoThich = (wchar_t*)malloc(a*sizeof(wchar_t));
+			fseek(FileIn, b + 1, SEEK_SET);
+			fgetws(DS-> SoThich, a, FileIn);
+			b = ftell(FileIn);
+			BatDau = b;
+		}
+		else
+		{
+			fseek(FileIn, b + 2, SEEK_SET);
+			a = LayViTri(FileIn, '"');
+			DS-> SoThich = (wchar_t*)malloc(a*sizeof(wchar_t));
+			fseek(FileIn, b + 2, SEEK_SET);
+			fgetws(DS-> SoThich, a, FileIn);
+
+			b = ftell(FileIn) + 1;
+
+		}
+		
+		BatDau = b + 2;
+	}
+	else
+	{
+		BatDau = b;
+	}
+	wprintf(L"%ls\n", DS-> MSSV);
+	wprintf(L"%ls\n", DS-> HoTen);
+	wprintf(L"%ls\n", DS-> khoa);
+	wprintf(L"%ls\n", DS-> KhoaHoc);
+	wprintf(L"%ls\n", DS-> NgaySinh);
+	wprintf(L"%ls\n", DS-> HinhAnh);
+	wprintf(L"%ls\n", DS-> MoTaBanThan);
+	wprintf(L"%ls\n", DS-> SoThich);
+}
+
+int dodai(wchar_t* s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
 		i++;
-	} while (1);
-	s[i] = 0;
-	return s;
-}
-
-int LayThongTin(FILE*fp, SinhVien& sv)
-{
-	DocThongTin(fp, sv.MSSV);
-	DocThongTin(fp, sv.HoVaTen);
-	DocThongTin(fp, sv.Khoa);
-	DocThongTin(fp, sv.KhoaHoc);
-	DocThongTin(fp, sv.NgaySinh);
-	DocThongTin(fp, sv.SoThich);
-	return 1;
-}
-
-void LaySinhVien(FILE*fp, SinhVien DS[], int&n)
-{
-	SinhVien sv; n = 0;
-	while (!feof(fp))
-	{
-		if (LayThongTin(fp, sv))
-		{
-			DS[n] = sv;
-			n++;
-		}
-		else break;
 	}
+	return i;
 }
 
-void In(FILE*fp, SinhVien DS[], int n)
+void saochepchuoi(wchar_t* str, wchar_t* s)
 {
-	for (int i = 0; i < n; i++)
+	int a = dodai(s);
+	for (int i = 0; i < a; i++)
 	{
-		fprintf(fp, "%s/t%15s %7s\t %s\t %s\t %s\n", DS[i].MSSV, DS[i].HoVaTen, DS[i].Khoa, DS[i].KhoaHoc, DS[i].NgaySinh, DS[i].SoThich);
+		str[i] = s[i];
+	}
+	str[a] = '\0';
+}
+void ghepchuoi(wchar_t* str, wchar_t* s)
+{
+	int a = dodai(str);
+	int m = dodai(str) + dodai(s);
+	for (int i = 0; i < m; i++)
+	{
+		if (i == a)
+		{
+			for (int j = 0; j < dodai(s); j++)
+			{
+				str[i] = s[j];
+				i++;
+			}
+		}
+	}
+	str[m] = '\0';
+}
+void ghifile(SinhVien* DS);
+void ghifile(SinhVien* DS)
+{
+	wchar_t filename[50];
+	saochepchuoi(filename, DS-> MSSV);
+	ghepchuoi(filename, L".html");
+	FILE* f;
+	_wfopen_s(&f, filename, L"wt,ccs=UTF-8");
+	_setmode(_fileno(f), _O_U8TEXT);
+	if (f == NULL)
+	{
+		wprintf(L"Tạo file lỗi!!!\n");
+	}
+	else
+	{
+		fwprintf(f, L"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n");
+		fwprintf(f, L"<html xmlns=\"http://www.w3.org/1999/xhtml\">\n");
+		fwprintf(f, L"	<head>\n");
+		fwprintf(f, L"		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n");
+		fwprintf(f, L"		<link rel=\"stylesheet\" type=\"text/css\" href=\"personal.css\" />\n");
+		fwprintf(f, L"		<title>HCMUS - %ls</title>\n", DS-> HoTen);
+		fwprintf(f, L"	</head>\n");
+		fwprintf(f, L"	<body>\n");
+		fwprintf(f, L"		<div class=\"Layout_container\">\n");
+		fwprintf(f, L"			<!-- Begin Layout Banner Region -->\n");
+		fwprintf(f, L"			<div class=\"Layout_Banner\" >\n");
+		fwprintf(f, L"				<div><img id=\"logo\" src=\"Images/logo.jpg\" height=\"105\" /></div>\n");
+		fwprintf(f, L"				<div class=\"Header_Title\">TRƯỜNG ÐẠI HỌC KHOA HỌC TỰ NHIÊN</div>\n");
+		fwprintf(f, L"			</div>\n");
+		fwprintf(f, L"			<!-- End Layout Banner Region -->\n");
+		fwprintf(f, L"			<!-- Begin Layout MainContents Region -->\n");
+		fwprintf(f, L"			<div class=\"Layout_MainContents\">\n");
+		fwprintf(f, L"				<!-- Begin Below Banner Region -->\n");
+		fwprintf(f, L"				<div class=\"Personal_Main_Information\">\n");
+		fwprintf(f, L"					<!-- Begin Thông tin cá nhân của sinh viên ------------------------------------------------------------------------------------------->\n");
+		fwprintf(f, L"					<div class=\"Personal_Location\"> \n");
+		fwprintf(f, L"						<img src=\"Images/LogoFooter.jpg\" width=\"27\" height=\"33\" />\n");
+		fwprintf(f, L"						<span class=\"Personal_FullName\">%ls - %ls</span>\n", DS-> HoTen, DS-> MSSV);
+		fwprintf(f, L"						<div class=\"Personal_Department\">%ls</div>\n", DS-> khoa);
+		fwprintf(f, L"						<br />\n");
+		fwprintf(f, L"						<div class=\"Personal_Phone\">\n");
+		fwprintf(f, L"						Email: \n");
+		fwprintf(f, L"						</div>\n");
+		fwprintf(f, L"						<br />\n");
+		fwprintf(f, L"						<br />\n");
+		fwprintf(f, L"					</div>\n");
+		fwprintf(f, L"					<!-- End Thông tin cá nhân của sinh viên ------------------------------------------------------------------------------------------->\n");
+		fwprintf(f, L"					<div class=\"Personal_HinhAnhKhung\">\n");
+		fwprintf(f, L"						<img src=\"Images/%ls\" class=\"Personal_HinhAnh\" />\n", DS-> HinhAnh);
+		fwprintf(f, L"					</div>\n");
+		fwprintf(f, L"				</div>\n");
+		fwprintf(f, L"				<!-- End Below Banner Region -->\n");
+		fwprintf(f, L"				<!-- Begin MainContents Region -->\n");
+		fwprintf(f, L"				<div class=\"MainContain\">\n");
+		fwprintf(f, L"\n");
+		fwprintf(f, L"					<!-- Begin Top Region -->\n");
+		fwprintf(f, L"					<div class=\"MainContain_Top\">\n");
+		fwprintf(f, L"\n");
+		fwprintf(f, L"						<div class=\"InfoGroup\">Thông tin cá nhân</div>\n");
+		fwprintf(f, L"                       <div>\n");
+		fwprintf(f, L"                            <ul class=\"TextInList\">\n");
+		fwprintf(f, L"                              <li>Họ và tên: %ls </li>\n", DS-> HoTen);
+		fwprintf(f, L"								 <li>MSSV: %ls </li>\n", DS-> MSSV);
+		fwprintf(f, L"								 <li>Sinh viên khoa: %ls </li>\n", DS-> khoa);
+		fwprintf(f, L"								 <li>Khóa Học: %ls </li>\n", DS-> KhoaHoc);
+		fwprintf(f, L"								 <li>Ngày sinh: %ls </li>\n", DS-> NgaySinh);
+		fwprintf(f, L"							 </ul>\n");
+		fwprintf(f, L"						</div>\n");
+		fwprintf(f, L"                       <div class=\"InfoGroup\">Sở thích</div>\n");
+		fwprintf(f, L"                       <div>\n");
+		fwprintf(f, L"                            <ul class=\"TextInList\">\n");
+		fwprintf(f, L"                              <li>%ls</li>\n", DS-> SoThich);
+		fwprintf(f, L"							 </ul>\n");
+		fwprintf(f, L"						</div>\n");
+		fwprintf(f, L"						<div class=\"InfoGroup\">Mô tả bản thân</div>\n");
+		fwprintf(f, L"						<div>\n");
+		fwprintf(f, L"							<ul class=\"Description\">\n");
+		fwprintf(f, L"                            <li>%ls</li>\n", DS-> MoTaBanThan);
+		fwprintf(f, L"							</ul>\n");
+		fwprintf(f, L"						</div>\n");
+		fwprintf(f, L"\n");
+		fwprintf(f, L"					</div>\n");
+		fwprintf(f, L"				</div>\n");
+		fwprintf(f, L"			</div>\n");
+		fwprintf(f, L"\n");
+		fwprintf(f, L"			<!-- Begin Layout Footer -->\n");
+		fwprintf(f, L"			<div class=\"Layout_Footer\">\n");
+		fwprintf(f, L"				<div class=\"divCopyright\">\n");
+		fwprintf(f, L"					<br />\n");
+		fwprintf(f, L"					<img src=\"Images/LogoFooter_gray.jpg\" width=\"34\" height=\"41\" /><br />\n");
+		fwprintf(f, L"					<br />\n");
+		fwprintf(f, L"				@2018</br>\n");
+		fwprintf(f, L"				Ðồ án giữa kì</br>\n");
+		fwprintf(f, L"				Kĩ thuật lập trình</br>\n");
+		fwprintf(f, L"				TH2018/04</br>\n");
+		fwprintf(f, L"				1712923 - Phạm Lê Hoài Vũ</br>\n");
+		fwprintf(f, L"				</div>\n");
+		fwprintf(f, L"			</div>\n");
+		fwprintf(f, L"			<!-- End Layout Footer -->\n");
+		fwprintf(f, L"		</div>\n");
+		fwprintf(f, L"	</body>\n");
+		fwprintf(f, L"</html>");
+		fclose(f);
 	}
 }
 
 void main()
 {
-	SinhVien DS[10]; int n;
-	FILE*fp = fopen("Book1.csv", "rt");
-	if (fp == NULL)
+	FILE* FileIn;
+	fopen_s(&FileIn, "SV.csv", "r, ccs=UTF-8");
+	if (FileIn == NULL)
 	{
-		return;
+		wprintf(L"FILE khong the mo\n");
 	}
-	LaySinhVien(fp, DS, n);
-	In(fp, DS, n);
+	else
+	{
+		int n = 0, BatDau = 3;
+		SinhVien* DS;
+		while (!feof(FileIn))
+		{
+			if (fgetwc(FileIn) == L'\n')
+			{
+				n++;
+			}
+		}
+		DS = (SinhVien*)malloc(sizeof(SinhVien)* n);
+		for (int i = 0; i <n; i++)
+		{
+			DocThongTin(FileIn, &DS[i], BatDau);
+			ghifile(&DS[i]);
+
+		}
+		free(DS);
+		fclose(FileIn);
+
+	}
 }
